2c3534abbf79155a5ba45a5a0dfbc1e3f7770863
Mochichi2003 <moyurusuto@gmail.com>
2021-01-10 21:51:39 +0900

    作ってるところ

--- components/Enlish-Lists.tsx
+++ components/Enlish-Lists.tsx
@@ -1,25 +1,34 @@
 import React from "react";
-import { Box, useColorModeValue, Heading } from "@chakra-ui/react";
-
+import { Box, useColorModeValue, Heading, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 const Main = () => {
   const headerBg = useColorModeValue("#fff", "gray.900");
 
   return (
-    <Box
-      w="100%"
-      boxShadow="base"
-      border="1px"
-      borderColor="gray.200"
-      my="4"
-      p="4"
-      rounded="md"
-      bg={headerBg}
-    >
-      <Heading as="h3" fontSize="2xl">
-        タイトル
-      </Heading>
-      わーい
-    </Box>
+    <NextLink href="/make">
+      <Link href="/make">
+        <Box
+          w="100%"
+          boxShadow="base"
+          border="1px"
+          borderColor={useColorModeValue("gray.200", "gray.600")}
+          my="4"
+          p="4"
+          rounded="md"
+          color={useColorModeValue("#000", "#fff")}
+          bg={headerBg}
+          transition="300ms"
+          _hover={{
+            bg: useColorModeValue("gray.100", "gray.700"),
+          }}
+        >
+          <Heading as="h3" mb="2" fontSize="2xl">
+            タイトル
+          </Heading>
+          わーい
+        </Box>
+      </Link>
+    </NextLink>
   );
 };
 
