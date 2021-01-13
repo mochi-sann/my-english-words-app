40dfbf76081c54da89f612bad064c7c2aef34d19
mochi-sann <moyurusuto@gmail.com>
2021-01-13 18:22:39 +0900

    よくわからん

--- components/makeForms/japaneseAndEnglish.tsx
+++ components/makeForms/japaneseAndEnglish.tsx
@@ -1,124 +1,11 @@
 import React from "react";
 
-import {
-  Input,
-  Box,
-  HStack,
-  InputGroup,
-  InputLeftAddon,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Radio,
-  RadioGroup,
-  FormHelperText,
-} from "@chakra-ui/react";
-import { Field, useField } from "react-final-form";
+import { Input, Box, HStack, FormControl } from "@chakra-ui/react";
+import { useField } from "react-final-form";
 // import { useForm } from "react-hook-form";
 
 type Inputs = {
   number: number;
-};
-
-const InputControl = ({ name, label, number }: any) => {
-  const { input, meta } = useField(name);
-  return (
-    <>
-      {/* <FormLabel htmlFor={name}>{label}</FormLabel> */}
-      <Input
-        {...input}
-        isInvalid={meta.error && meta.touched}
-        id={name}
-        placeholder={label}
-        defaultValue={"Japanese:" + number}
-        w="100%"
-        h="40px"
-        // name={"Japanese-asdfasdfa"}
-        // placeholder={"日本語:" + number}
-      />
-      {/* <Error name={name} /> */}
-    </>
-  );
-};
-
-// const Error = ({ name }) => {
-//   const {
-//     meta: { error },
-//   } = useField(name, { subscription: { error: true } });
-//   return <FormErrorMessage>{error}</FormErrorMessage>;
-// };
-const Main = ({ number }: Inputs) => {
-  return (
-    <>
-      <HStack
-        w="100%"
-        pt="2"
-        pb="2"
-        marginTop="0px"
-        borderTop="1px"
-        borderTopColor="gray.200"
-      >
-        {/* <Box px="0" w="20px" textAlign="center">
-          {number}
-        </Box> */}
-        <InputGroup w="50%">
-          <InputLeftAddon mr="2">{number}</InputLeftAddon>
-          {/* inputのところ */}
-          <InputControl name="222" label={"日本語:" + number} number={number} />
-
-          {/* <Input
-            defaultValue={"Japanese:" + number}
-            w="100%"
-            h="40px"
-            name={"Japanese-" + number}
-            placeholder={"日本語:" + number}
-          /> */}
-        </InputGroup>
-        <Box px="0px" ml="0" w="50%">
-          <Input
-            defaultValue={"Englsh:" + number}
-            w="100%"
-            h="40px"
-            // px="0px"
-            ml="0"
-            name={"Englsh-" + number}
-            placeholder={"Englsh:" + number}
-          />
-        </Box>
-      </HStack>
-      {/* <InputControl name="lastName" label="Last Name" /> */}
-
-      <div>
-        <label>
-          <Field
-            name={"Englsh-" + number}
-            component="input"
-            type="text"
-            value="larry"
-          />{" "}
-          Larry
-        </label>
-        <label>
-          <Field
-            name={"Englsh-" + number + 1}
-            component="input"
-            type="text"
-            value="moe"
-          />{" "}
-          Moe
-        </label>
-        <label>
-          <Field
-            name={"Englsh-" + number + 2}
-            component="input"
-            type="text"
-            value="curly"
-          />{" "}
-          Curly
-        </label>
-      </div>
-    </>
-  );
 };
 
 type WordinputProps = {
@@ -135,7 +22,7 @@ const Wordinput = ({ lang, number }: WordinputProps) => {
         isInvalid={meta.error && meta.touched}
         id={lang + ":" + number}
         placeholder={lang + ":" + number}
-        defaultValue={lang + ":" + number}
+        defaultValue={"lang" + ":" + "number"}
         w="100%"
         h="40px"
         // name={"Japanese-asdfasdfa"}
--- pages/make.tsx
+++ pages/make.tsx
@@ -1,15 +1,29 @@
 import Layout from "../components/Layout";
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 // import { useForm } from "react-hook-form";
-import { Form } from "react-final-form";
+import { Form, useField, Field } from "react-final-form";
+import arrayMutators from "final-form-arrays";
+import { FieldArray } from "react-final-form-arrays";
 
-import { Box, Button, Code, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Code,
+  Heading,
+  NumberInput,
+  NumberInputField,
+  IconButton,
+  Input,
+  HStack,
+} from "@chakra-ui/react";
+
 // import { db } from "~/lib/firebase.ts";
 
 import FromInputs from "~/components/makeForms/japaneseAndEnglish.tsx";
 
 import { TopcoatPlus, TopcoatMinus } from "~/components/svgs/icon.tsx";
+// import { type } from "os";
 
 // type Inputs = {
 //   example: string;
@@ -28,23 +42,99 @@ const makePage = () => {
     </Layout>
   );
 };
+type InputCountsProps = { InputCounts: number };
+const InputCountsvalue = ({ InputCounts }: InputCountsProps) => {
+  const { input, meta } = useField("lang +  + number");
 
+  return (
+    <>
+      <Input
+        {...input}
+        isInvalid={meta.error && meta.touched}
+        placeholder="Basic usage"
+        value={InputCounts}
+      />
+    </>
+  );
+};
 const MyForm = () => {
   const onSubmit = async (values: any) => {
     // await sleep(300);
     window.alert(JSON.stringify(values, 2, 2));
     console.log(JSON.stringify(values, 2, 2));
   };
+
+  const [InputCounts, setInputCounts] = useState(10);
+
+  useEffect(() => {
+    console.log(`InputCounts : ${InputCounts} `);
+    if (InputCounts > 100) {
+      setInputCounts(100);
+    } else if (InputCounts < 1) {
+      setInputCounts(1);
+    }
+  });
+
+  // const { input, meta } = useField("InputCounts");
+
   return (
     <Form
       onSubmit={onSubmit}
       initialValues={{ stooge: "larry", employed: false }}
       render={({ handleSubmit, form, values }) => (
         <form onSubmit={handleSubmit}>
-          <FromInputs number={1} />
+          {/* <FromInputs number={1} />
           <FromInputs number={2} />
           <FromInputs number={3} />
-          <FromInputs number={4} />
+          <FromInputs number={4} /> */}
+
+          {(() => {
+            // 英に日本語の問題集を作るInputを入れるとこ
+            const items = [];
+            for (let i = 1; i <= InputCounts; i++) {
+              items.push(
+                <>
+                  <FromInputs number={i} />
+                </>
+              );
+            }
+            return <>{items}</>;
+          })()}
+
+          <HStack>
+            <Input placeholder="Basic usage" value={InputCounts} />
+            <InputCountsvalue InputCounts={InputCounts} />
+            <Button
+              leftIcon={<TopcoatPlus />}
+              colorScheme="blue"
+              m="2"
+              onClick={() => setInputCounts(InputCounts + 10)}
+            >
+              10
+            </Button>
+            <IconButton
+              aria-label="行を追加"
+              icon={<TopcoatPlus />}
+              colorScheme="blue"
+              m="2"
+              onClick={() => setInputCounts(InputCounts + 1)}
+            />
+            <IconButton
+              aria-label="行を削除"
+              icon={<TopcoatMinus />}
+              colorScheme="blue"
+              m="2"
+              onClick={() => setInputCounts(InputCounts - 1)}
+            />
+            <Button
+              leftIcon={<TopcoatMinus />}
+              colorScheme="blue"
+              m="2"
+              onClick={() => setInputCounts(InputCounts - 10)}
+            >
+              10
+            </Button>
+          </HStack>
 
           <div className="buttons">
             <Button
@@ -52,7 +142,7 @@ const MyForm = () => {
               type="submit"
               // disabled={submitting || pristine}
             >
-              Submit
+              決定
             </Button>
             <Button
               m={4}
@@ -60,7 +150,7 @@ const MyForm = () => {
               onClick={form.reset}
               // disabled={submitting || pristine}
             >
-              Reset
+              リセット
             </Button>
           </div>
           <Code>{JSON.stringify(values, 2, 2)}</Code>
