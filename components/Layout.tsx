0bfe228b9482a4cc19e5b16c455c377387ce47f5
mochi-sann <moyurusuto@gmail.com>
2021-01-15 14:42:43 +0900

    next headの各場所をLayoutないから別ファイルにした

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 import NextLink from "next/link";
 import Image from "next/image";
-import Head from "next/head";
+
 import {
   Flex,
   Spacer,
@@ -24,6 +24,7 @@ import AuthContext from "~/lib/AuthContext";
 import LoginWithGoogle, { LoginBtn } from "@components/LoginAndLogoutButtom";
 
 import { FaSolidMoon, IcRoundWbSunny } from "~/components/svgs/icon";
+import HeadsDeta from "~/components/HeadsDetas.tsx";
 
 function ChangeDarkModeAndLightMode() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -38,78 +39,6 @@ function ChangeDarkModeAndLightMode() {
     </>
   );
 }
-
-type HeadsDetaPropsType = {
-  children?: ReactNode;
-  title?: string;
-};
-
-const HeadsDeta = ({ children, title }: HeadsDetaPropsType) => {
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
-      {/* For new browsers - multisize ico  */}
-      <link
-        rel="icon"
-        type="image/x-icon"
-        sizes="16x16 32x32"
-        href="favicon.ico"
-      />
-      {/* For iPad with high-resolution Retina display running iOS ≥ 7: */}
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="icons/favicon-152-precomposed.png"
-      />
-      {/* For iPad with high-resolution Retina display running iOS ≤ 6: */}
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href="icons/favicon-144-precomposed.png"
-      />
-      {/* For iPhone with high-resolution Retina display running iOS ≥ 7: */}
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="icons/favicon-120-precomposed.png"
-      />
-      {/* For iPhone with high-resolution Retina display running iOS ≤ 6: */}
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="icons/favicon-114-precomposed.png"
-      />
-      {/* For iPhone 6+ */}
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="icons/favicon-180-precomposed.png"
-      />
-      {/* For first- and second-generation iPad: */}
-      <link
-        rel="apple-touch-icon"
-        sizes="72x72"
-        href="icons/favicon-72-precomposed.png"
-      />
-      {/* For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: */}
-      <link rel="apple-touch-icon" sizes="57x57" href="icons/favicon-57.png" />
-      {/* For Old Chrome */}
-      <link rel="icon" sizes="32x32" href="icons/favicon-32.png" />
-      {/* For IE10 Metro */}
-      <meta name="msapplication-TileColor" content="#FFFFFF" />
-      <meta name="msapplication-TileImage" content="icons/favicon-144.png" />
-      <meta name="theme-color" content="#ffffff" />
-      {/* Chrome for Android */}
-      <link rel="manifest" href="icons/manifest.json" />
-      <link rel="icon" sizes="192x192" href="icons/favicon-192.png" />
-      {children}
-    </Head>
-  );
-};
 
 type Props = {
   children?: ReactNode;
