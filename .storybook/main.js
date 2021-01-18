ee40e2899b1b0f67bbf6f55c88102939b4611975
mochi-sann <moyurusuto@gmail.com>
2021-01-18 20:22:32 +0900

    storybookのセットアップができた

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,48 +1,28 @@
-// module.exports = {
-//   stories: [
-//     "../stories/**/*.stories.mdx",
-//     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-//   ],
-// };
-
 const path = require("path");
 
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
-  stories: [
-    "../components/**/**/*.stories.tsx",
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  stories: ["../**/*.stories.tsx"],
+  addons: [
+    "storybook-addon-performance/register",
+    "@storybook/addon-a11y",
+    "@storybook/addon-toolbars",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  typescript: {
+    reactDocgen: false,
+  },
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("awesome-typescript-loader"),
-          options: {
-            configFileName: path.resolve(__dirname, "./tsconfig.json"),
-          },
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
         },
-        /* 
-          ** OPTIONAL ** 
-    						 
-          Basically a webpack loader  used to 				 
-          generate docgen information from TypeScript React components. 
-The primary use case is to get the prop types 
-table populated in the Storybook Info Addon.
-    	*/
-        {
-          loader: require.resolve("react-docgen-typescript-loader"),
-          options: {
-            tsconfigPath: path.resolve(__dirname, "./tsconfig.json"),
-          },
-        },
-      ],
-    });
-
-    config.resolve.extensions.push(".ts", ".tsx");
-
-    return config;
+      },
+    };
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,61 +1,69 @@
 // import "../styles/global.css";
 
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
 import "../styles/global.css";
-
-import React from "react";
-import { addDecorator } from "@storybook/react";
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-};
-
-const styles = {
-  global: (props) => ({
-    body: {
-      color: mode("gray.800", "#f8f8f2")(props),
-      bg: mode("white", "#282a36")(props),
-    },
-  }),
-};
-
-const components = {
-  Link: {
-    // setup light/dark mode component defaults
-    baseStyle: (props) => ({
-      color: mode("blue.400", "#8be9fd")(props),
-    }),
-  },
-};
-
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  DraculaBackground: "#282a36",
-  DraculaCurrent_Line: "#44475a",
-  DraculaForeground: "#f8f8f2",
-  DraculaComment: "#6272a4",
-  DraculaCyan: "#8be9fd",
-  DraculaGreen: "#50fa7b",
-  DraculaOrange: "#ffb86c",
-  DraculaPink: "#ff79c6",
-  DraculaPurple: "#bd93f9",
-  DraculaRed: "#ff5555",
-  DraculaYellow: "#f1fa8c",
-
-  testRed: "#ff0000",
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors, components, styles });
-
-addDecorator((storyFn) => (
-  <ChakraProvider theme={theme}>{storyFn()}</ChakraProvider>
-));
 
 // export const parameters = {
 //   actions: { argTypesRegex: "^on[A-Z].*" },
 // };
+import {
+  ChakraProvider,
+  extendTheme,
+  Flex,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { withPerformance } from "storybook-addon-performance";
+
+/**
+ * Add global context for RTL-LTR switching
+ */
+export const globalTypes = {
+  direction: {
+    name: "Direction",
+    description: "Direction for layout",
+    defaultValue: "LTR",
+    toolbar: {
+      icon: "globe",
+      items: ["LTR", "RTL"],
+    },
+  },
+};
+
+const ColorModeToggleBar = () => {
+  const { toggleColorMode } = useColorMode();
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const nextMode = useColorModeValue("dark", "light");
+
+  return (
+    <Flex justify="flex-end" mb={4}>
+      <IconButton
+        size="md"
+        fontSize="lg"
+        aria-label={`Switch to ${nextMode} mode`}
+        variant="ghost"
+        color="current"
+        marginLeft="2"
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+      />
+    </Flex>
+  );
+};
+
+const withChakra = (StoryFn, context) => {
+  const { direction } = context.globals;
+  const dir = direction.toLowerCase();
+  return (
+    <ChakraProvider theme={extendTheme({ direction: dir })}>
+      <div dir={dir} id="story-wrapper" style={{ minHeight: "100vh" }}>
+        <ColorModeToggleBar />
+        <StoryFn />
+      </div>
+    </ChakraProvider>
+  );
+};
+
+export const decorators = [withChakra, withPerformance];
