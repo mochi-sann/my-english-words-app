7e4add67d14763ce8382224a5c7f1c30a5cb44b2
mochi-sann <moyurusuto@gmail.com>
2021-01-31 18:55:03 +0900

    多分できた

---
+++ components/examinations/examination.tsx
@@ -0,0 +1,100 @@
+// import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { db, auth } from "~/lib/firebase";
+import { useDocument } from "react-firebase-hooks/firestore";
+import { Box, Text, HStack, Divider, Heading, Button } from "@chakra-ui/react";
+
+import Layout from "~/components/Layout/Layout";
+import LoadingBox from "~/components/LoadingPeage/LoadingPeage";
+import ExamJaEn from "~/components/examJaEn/ExamJaEn";
+
+type Props = { docName: any };
+const Post = ({ docName }: Props) => {
+  // const router = useRouter();
+  // const { docName } = router.query;
+  const user = auth!.currentUser;
+  if (user) {
+    const [CheckAnswer, setCheckAnswer] = useState(false);
+    const userUid = user!.uid || 123;
+    // if (user) {
+    const [value, loading, error] = useDocument(
+      db.doc("lists/" + userUid + "/" + userUid + "/" + docName),
+      {
+        snapshotListenOptions: { includeMetadataChanges: true },
+      }
+    );
+
+    return (
+      <Layout>
+        {/* <p>{user!.uid!}</p> */}
+        {/* <p>Post: {JSON.stringify(docName)}</p> */}
+        <Heading mt="4" mb="6">
+          {/* {value.data().values.title} */}
+        </Heading>
+        <Box>
+          {/* {JSON.stringify(value, null, 2)} */}
+          {error && <strong>Error: {JSON.stringify(error)}</strong>}
+          {loading && <LoadingBox />}
+        </Box>
+        <Box>
+          {value && (
+            <Box>
+              {/* 問題を並べるところ */}
+              <HStack w="100%">
+                <Text w="50%">日本語</Text>
+                <Text w="50%">答え</Text>
+              </HStack>
+              {value.data().values.collection.map((doc: any) => (
+                <React.Fragment key={doc.id}>
+                  {/* <Box className="bg-gray-200 p-2 rounded-lg my-2" as="pre">
+                      {JSON.stringify(doc, null, 2)}
+                    </Box> */}
+                  <Divider my="2" />
+                  {/* <HStack w="100%">
+                      <Text w="50%">{doc.japanese}</Text>
+                      <Input w="50%" />
+                    </HStack> */}
+                  <ExamJaEn
+                    English={doc.english}
+                    japanese={doc.japanese}
+                    ShowAnswer={CheckAnswer}
+                  />
+                </React.Fragment>
+              ))}
+            </Box>
+          )}
+        </Box>
+        <Button
+          isChecked={CheckAnswer}
+          onClick={() => {
+            if (CheckAnswer) {
+              setCheckAnswer(false);
+            } else {
+              setCheckAnswer(true);
+            }
+          }}
+          onChange={(e) => {
+            setCheckAnswer(true);
+          }}
+          my="2"
+        >
+          答えを
+          {CheckAnswer ? "非表示" : "表示"}
+        </Button>
+        {/* <Box className="bg-gray-200 p-2 rounded-lg" mt="6" as="pre">
+          {value && (
+            <span>Document: {JSON.stringify(value.data(), null, 2)}</span>
+          )}
+        </Box> */}
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <p>ログインしていません</p>
+      </Layout>
+    );
+  }
+};
+
+export default Post;
--- pages/examination/[docName].tsx
+++ pages/examination/[docName].tsx
@@ -1,81 +1,92 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 
-import { useDocument } from "react-firebase-hooks/firestore";
-import { Box, Text, HStack, Divider, Heading } from "@chakra-ui/react";
-
-import Layout from "~/components/Layout/Layout";
-import LoadingBox from "~/components/LoadingPeage/LoadingPeage";
-import ExamJaEn from "~/components/examJaEn/ExamJaEn";
-import { db, auth } from "~/lib/firebase";
+// import { useDocument } from "react-firebase-hooks/firestore";
+// import { Box, Text, HStack, Divider, Heading } from "@chakra-ui/react";
+//
+// import Layout from "~/components/Layout/Layout";
+// import LoadingBox from "~/components/LoadingPeage/LoadingPeage";
+// import ExamJaEn from "~/components/examJaEn/ExamJaEn";
+import Examination from "~/components/examinations/examination";
+// import { db, auth } from "~/lib/firebase";
 
 const Post = () => {
   const router = useRouter();
   const { docName } = router.query;
-  const user = auth!.currentUser;
-  const [CheckAnswer /* setCheckAnswer */] = useState(false);
-  if (user) {
-    const [value, loading, error] = useDocument(
-      db.doc("lists/" + user!.uid + "/" + user!.uid + "/" + docName),
-      {
-        snapshotListenOptions: { includeMetadataChanges: true },
-      }
-    );
-
-    return (
-      <Layout>
-        {/* <p>{user!.uid!}</p> */}
-        {/* <p>Post: {JSON.stringify(docName)}</p> */}
-        <Heading mt="4" mb="6">
-          {value.data().values.title}
-        </Heading>
-        <Box>
-          {/* {JSON.stringify(value, null, 2)} */}
-          {error && <strong>Error: {JSON.stringify(error)}</strong>}
-          {loading && <LoadingBox />}
-        </Box>
-        <Box>
-          {value && (
-            <Box>
-              {/* 問題を並べるところ */}
-              <HStack w="100%">
-                <Text w="50%">日本語</Text>
-                <Text w="50%">答え</Text>
-              </HStack>
-              {value.data().values.collection.map((doc: any) => (
-                <React.Fragment key={doc.id}>
-                  {/* <Box className="bg-gray-200 p-2 rounded-lg my-2" as="pre">
-                    {JSON.stringify(doc, null, 2)}
-                  </Box> */}
-                  <Divider my="2" />
-                  {/* <HStack w="100%">
-                    <Text w="50%">{doc.japanese}</Text>
-                    <Input w="50%" />
-                  </HStack> */}
-                  <ExamJaEn
-                    English={doc.english}
-                    japanese={doc.japanese}
-                    ShowAnswer={CheckAnswer}
-                  />
-                </React.Fragment>
-              ))}
-            </Box>
-          )}
-        </Box>
-        <Box className="bg-gray-200 p-2 rounded-lg" mt="6" as="pre">
-          {value && (
-            <span>Document: {JSON.stringify(value.data(), null, 2)}</span>
-          )}
-        </Box>
-      </Layout>
-    );
-  } else {
-    return (
-      <Layout>
-        <p>ログインしていません</p>
-      </Layout>
-    );
-  }
+  return (
+    <div>
+      <Examination docName={docName} />
+    </div>
+  );
 };
+// const Post = () => {
+//   const router = useRouter();
+//   const { docName } = router.query;
+//   const user = auth!.currentUser;
+//   const [CheckAnswer /* setCheckAnswer */] = useState(false);
+
+//   if (user) {
+//     const [value, loading, error] = useDocument(
+//       db.doc("lists/" + user!.uid + "/" + user!.uid + "/" + docName),
+//       {
+//         snapshotListenOptions: { includeMetadataChanges: true },
+//       }
+//     );
+
+//     return (
+//       <Layout>
+//         {/* <p>{user!.uid!}</p> */}
+//         {/* <p>Post: {JSON.stringify(docName)}</p> */}
+//         <Heading mt="4" mb="6">
+//           {value.data().values.title}
+//         </Heading>
+//         <Box>
+//           {/* {JSON.stringify(value, null, 2)} */}
+//           {error && <strong>Error: {JSON.stringify(error)}</strong>}
+//           {loading && <LoadingBox />}
+//         </Box>
+//         <Box>
+//           {value && (
+//             <Box>
+//               {/* 問題を並べるところ */}
+//               <HStack w="100%">
+//                 <Text w="50%">日本語</Text>
+//                 <Text w="50%">答え</Text>
+//               </HStack>
+//               {value.data().values.collection.map((doc: any) => (
+//                 <React.Fragment key={doc.id}>
+//                   {/* <Box className="bg-gray-200 p-2 rounded-lg my-2" as="pre">
+//                     {JSON.stringify(doc, null, 2)}
+//                   </Box> */}
+//                   <Divider my="2" />
+//                   {/* <HStack w="100%">
+//                     <Text w="50%">{doc.japanese}</Text>
+//                     <Input w="50%" />
+//                   </HStack> */}
+//                   <ExamJaEn
+//                     English={doc.english}
+//                     japanese={doc.japanese}
+//                     ShowAnswer={CheckAnswer}
+//                   />
+//                 </React.Fragment>
+//               ))}
+//             </Box>
+//           )}
+//         </Box>
+//         <Box className="bg-gray-200 p-2 rounded-lg" mt="6" as="pre">
+//           {value && (
+//             <span>Document: {JSON.stringify(value.data(), null, 2)}</span>
+//           )}
+//         </Box>
+//       </Layout>
+//     );
+//   } else {
+//     return (
+//       <Layout>
+//         <p>ログインしていません</p>
+//       </Layout>
+//     );
+//   }
+// };
 
 export default Post;
